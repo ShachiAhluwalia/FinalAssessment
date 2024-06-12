aab5561a618c1635b6cc60ec822b51fead89748f
ShachiAhluwalia <138997798+ShachiAhluwalia@users.noreply.github.com>
2024-06-12 15:04:09 +0530

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,43 @@
+<h1>Data Science Tools and Ecosystem</h1>
+<p>In this notebook, Data Science Tools and Ecosystem are summarized.</p>
+<p>Some of the popular languages that Data Scientists use are:</p>
+<ol>
+    <li>Python</li>
+    <li>R Language</li>
+    <li>Java Script</li>
+    <li>Scala</li>
+</ol>
+<p>Some of the commonly used libraries used by Data Scientists include:</p>
+<ol>
+    <li>Matplotlib</li>
+    <li>ggplot</li>
+    <li>Seaborn</li>
+    <li>Plotly</li>
+</ol>
+
+| Data Science Tools |
+|--------------------|
+| BigML              |
+| Apache Spark       |
+| SAS                |
+
+
+<h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+code = compile("5 + 8", "<string>", "eval")
+eval(code)
+
+code1 = compile("(9 + 7) * 5", "<string>", "eval")
+eval(code1)
+
+import math
+# Volume of a sphere
+code2 = compile("5 / 2 * math.pi * math.pow(25, 3)", "<string>", "eval")
+eval(code2)
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+#This will convert 200 minutes to hours by diving by 60
+
+time_in_hours=200/60
+print(time_in_hours,"hours")
